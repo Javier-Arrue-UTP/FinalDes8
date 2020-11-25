958b2195ff34845cc01a515953e4c845653ff6a2
Javier Arrue <armandoutp1@gmail.com>
2020-11-25 12:30:30 -0500

    Yep

--- ConexionBD.vb
+++ ConexionBD.vb
@@ -1,25 +1,5 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class ConexionBD
-<<<<<<< HEAD
-    Public conexion As New SqlConnection("Data Source=DESKTOP-UH7R3KP;Initial Catalog=finalDes5;Integrated Security=True")
-=======
-    Dim conexion As New SqlConnection("Data Source=RYL;Initial Catalog=finalDes5;Integrated Security=True")
 
->>>>>>> 25a4d8e5464e4ed60bc4daa08f88b94f1e8131b9
-    Sub enlace()
-        Try
-            conexion.Open()
-
-        Catch ex As Exception
-        End Try
-    End Sub
-
-    Sub cerrar()
-        conexion.Close()
-    End Sub
-<<<<<<< HEAD
-
-=======
->>>>>>> 25a4d8e5464e4ed60bc4daa08f88b94f1e8131b9
 End Class
